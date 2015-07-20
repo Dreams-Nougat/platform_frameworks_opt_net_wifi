9d70bc50e0786328fe161773b539763a23bb5370
Alice Liu <alice.liu@intel.com>
2015-07-20 15:24:44 +0800

    WiFi: Make WiFi scan as the only one scan interval period
    When WiFi is disconnected and the screen is on, it is found
    that WiFi scans as two interval periods. when the message
    CMD_NO_NETWORKS_PERIODIC_SCAN is processed in DisconnectedState,
    it scans as supplicant scan interval 15s. When the message
    CMD_START_SCAN is processed in DisconnectedState, it scans
    as disconnected scan interval 10s. These two scan interval
    periods exist at the same time. The other issue is that when
    CMD_START_SCAN message is obsolete, background scan is disabled.
    This will cause PNO does not work at that time. This patch
    fixes the above two issues.
    
    Change-Id: I052face1b01c0cb022c8224ad86f14afde5cd767
    Signed-off-by: Alice Liu <alice.liu@intel.com>

--- service/java/com/android/server/wifi/WifiStateMachine.java
+++ service/java/com/android/server/wifi/WifiStateMachine.java
@@ -8059,19 +8059,12 @@ public class WifiStateMachine extends StateMachine {
                         messageHandlingStatus = MESSAGE_HANDLING_STATUS_REFUSED;
                         return HANDLED;
                     }
-                    /* Disable background scan temporarily during a regular scan */
-                    if (mEnableBackgroundScan) {
-                        enableBackgroundScan(false);
-                    }
-                    if (message.arg1 == SCAN_ALARM_SOURCE) {
+                    if (message.arg1 == SCAN_ALARM_SOURCE && mP2pConnected.get()) {
                         // Check if the CMD_START_SCAN message is obsolete (and thus if it should
                         // not be processed) and restart the scan
-                        int period =  mDisconnectedScanPeriodMs;
-                        if (mP2pConnected.get()) {
-                           period = (int)Settings.Global.getLong(mContext.getContentResolver(),
+                        int period = (int)Settings.Global.getLong(mContext.getContentResolver(),
                                     Settings.Global.WIFI_SCAN_INTERVAL_WHEN_P2P_CONNECTED_MS,
                                     mDisconnectedScanPeriodMs);
-                        }
                         if (!checkAndRestartDelayedScan(message.arg2,
                                 true, period, null, null)) {
                             messageHandlingStatus = MESSAGE_HANDLING_STATUS_OBSOLETE;
@@ -8081,9 +8074,17 @@ public class WifiStateMachine extends StateMachine {
                                     + " -> obsolete");
                             return HANDLED;
                         }
+                        /* Disable background scan temporarily during a regular scan */
+                        if (mEnableBackgroundScan) {
+                             enableBackgroundScan(false);
+                        }
                         handleScanRequest(WifiNative.SCAN_WITHOUT_CONNECTION_SETUP, message);
                         ret = HANDLED;
                     } else {
+                        /* Disable background scan temporarily during a regular scan */
+                        if (mEnableBackgroundScan) {
+                             enableBackgroundScan(false);
+                        }
                         ret = NOT_HANDLED;
                     }
                     break;
