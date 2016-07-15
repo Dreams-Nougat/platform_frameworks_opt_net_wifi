575c53f5c82d678cad2c65e5853a21a56b7fbfbb
zhangshuxiao <zhangshuxiao@xiaomi.com>
2016-07-15 10:47:29 +0800

    Some networks can not connect automatically.
    Becasuse the key of scan cache is supplicant's networkid,
    when supplicant process restart, the supplicant's networkid
    may be regenerated, so clear scan cache when loading the
    configured networks.
    
    Change-Id: Iec94e8eef517bd0411eb0be53a31cdb34a1494cf
    Signed-off-by: zhangshuxiao <zhangshuxiao@xiaomi.com>

--- service/java/com/android/server/wifi/WifiConfigStore.java
+++ service/java/com/android/server/wifi/WifiConfigStore.java
@@ -1792,6 +1792,7 @@ public class WifiConfigStore extends IpConfigStore {
         mLastPriority = 0;
 
         mConfiguredNetworks.clear();
+        mScanDetailCaches.clear();
         List<WifiConfiguration> configTlsResetList = new ArrayList<WifiConfiguration>();
         int last_id = -1;
         boolean done = false;
