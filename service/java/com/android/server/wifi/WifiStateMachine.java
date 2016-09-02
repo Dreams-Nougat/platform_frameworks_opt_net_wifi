694f81788308690a7e22f8ef092018b6063e67b2
Rebecca Silberstein <silberst@google.com>
2016-09-02 14:55:12 -0700

    WifiInjector: add WifiApConfigStore to injector
    Move creation of the WifiApConfigStore to WifiInjector instead of
    creating the WifiApConfigStore in WifiStateMachine.
    
    Bug: 31267375
    Test: frameworks/opt/net/wifi/tests/wifitests/runtests.sh
    Test: manually started tethering with and without a sim
    Test: started tethering, changed ap config and softap restarted
    Change-Id: I801d1861fdf003a9db1068c1f52a430decb24c94

--- service/java/com/android/server/wifi/WifiStateMachine.java
+++ service/java/com/android/server/wifi/WifiStateMachine.java
@@ -3957,8 +3957,7 @@ public class WifiStateMachine extends StateMachine implements WifiNative.WifiRss
             }
 
             if (mWifiApConfigStore == null) {
-                mWifiApConfigStore =
-                        mFacade.makeApConfigStore(mContext, mBackupManagerProxy);
+                mWifiApConfigStore = mWifiInjector.getWifiApConfigStore();
             }
         }
         @Override
@@ -6648,11 +6647,9 @@ public class WifiStateMachine extends StateMachine implements WifiNative.WifiRss
             }
 
             checkAndSetConnectivityInstance();
-            mSoftApManager = mWifiInjector.makeSoftApManager(
-                    mWifiNative, mNwService,
-                    mCm, mCountryCode.getCountryCode(),
-                    mWifiApConfigStore.getAllowed2GChannel(),
-                    new SoftApListener(), apInterface);
+            mSoftApManager = mWifiInjector.makeSoftApManager(mNwService,
+                                                             new SoftApListener(),
+                                                             apInterface);
             mSoftApManager.start(config);
         }
 
