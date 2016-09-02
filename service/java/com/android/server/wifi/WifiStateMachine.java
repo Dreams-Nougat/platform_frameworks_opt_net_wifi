615f28492b31542db12fdecb1df23b9352d7670a
Randy Pan <zpan@google.com>
2016-09-02 15:15:53 -0700

    Wifi Network Selector
    Set up the layout of the new Wifi Network Selector. Add the implementation
    of Save Network Evaluator and Exernally Scored Network Evaluator.
    
    Bug: 31089538
    Change-Id: Ieb269060669df220c3d1777eb3aac3b119d4fdc1

--- service/java/com/android/server/wifi/WifiStateMachine.java
+++ service/java/com/android/server/wifi/WifiStateMachine.java
@@ -193,7 +193,7 @@ public class WifiStateMachine extends StateMachine implements WifiNative.WifiRss
     private WifiConfigManager mWifiConfigManager;
     private WifiSupplicantControl mWifiSupplicantControl;
     private WifiConnectivityManager mWifiConnectivityManager;
-    private WifiQualifiedNetworkSelector mWifiQualifiedNetworkSelector;
+    private WifiNetworkSelector mWifiNetworkSelector;
     private INetworkManagementService mNwService;
     private IWificond mWificond;
     private IClientInterface mClientInterface;
@@ -885,8 +885,8 @@ public class WifiStateMachine extends StateMachine implements WifiNative.WifiRss
         mWifiDiagnostics = mWifiInjector.makeWifiDiagnostics(mWifiNative);
 
         mWifiInfo = new WifiInfo();
-        mWifiQualifiedNetworkSelector = new WifiQualifiedNetworkSelector(mWifiConfigManager,
-                mContext, mWifiInfo, mWifiInjector.getClock());
+        mWifiNetworkSelector = new WifiNetworkSelector(mContext, mWifiConfigManager,
+                mWifiInfo, mWifiInjector.getClock());
         mSupplicantStateTracker =
                 mFacade.makeSupplicantStateTracker(context, mWifiConfigManager, getHandler());
 
@@ -1172,7 +1172,7 @@ public class WifiStateMachine extends StateMachine implements WifiNative.WifiRss
         mWifiNative.enableVerboseLogging(verbose);
         mWifiConfigManager.enableVerboseLogging(verbose);
         mSupplicantStateTracker.enableVerboseLogging(verbose);
-        mWifiQualifiedNetworkSelector.enableVerboseLogging(verbose);
+        mWifiNetworkSelector.enableVerboseLogging(verbose);
         if (mWifiConnectivityManager != null) {
             mWifiConnectivityManager.enableVerboseLogging(verbose);
         }
@@ -2152,7 +2152,7 @@ public class WifiStateMachine extends StateMachine implements WifiNative.WifiRss
         pw.println();
         mWifiDiagnostics.captureBugReportData(WifiDiagnostics.REPORT_REASON_USER_ACTION);
         mWifiDiagnostics.dump(fd, pw, args);
-        mWifiQualifiedNetworkSelector.dump(fd, pw, args);
+        mWifiNetworkSelector.dump(fd, pw, args);
         dumpIpManager(fd, pw, args);
         if (mWifiConnectivityManager != null) {
             mWifiConnectivityManager.dump(fd, pw, args);
@@ -2750,9 +2750,6 @@ public class WifiStateMachine extends StateMachine implements WifiNative.WifiRss
 
         if (mWifiNative.setBand(band)) {
             mFrequencyBand.set(band);
-            if (mWifiConnectivityManager != null) {
-                mWifiConnectivityManager.setUserPreferredBand(band);
-            }
             if (mVerboseLoggingEnabled) {
                 logd("done set frequency band " + band);
             }
@@ -4223,7 +4220,7 @@ public class WifiStateMachine extends StateMachine implements WifiNative.WifiRss
                 synchronized (mWifiReqCountLock) {
                     mWifiConnectivityManager = new WifiConnectivityManager(mContext,
                         WifiStateMachine.this, mWifiScanner, mWifiConfigManager, mWifiInfo,
-                        mWifiQualifiedNetworkSelector, mWifiInjector,
+                        mWifiNetworkSelector, mWifiInjector,
                         getHandler().getLooper(), hasConnectionRequests());
                     mWifiConnectivityManager.setUntrustedConnectionAllowed(mUntrustedReqCount > 0);
                     mWifiConnectivityManager.handleScreenStateChanged(mScreenOn);
@@ -6271,7 +6268,7 @@ public class WifiStateMachine extends StateMachine implements WifiNative.WifiRss
                                 || (lastRoam > 0 && lastRoam < 2000) /* unless driver is roaming */)
                             && ((ScanResult.is24GHz(mWifiInfo.getFrequency())
                                     && mWifiInfo.getRssi() >
-                                    WifiQualifiedNetworkSelector.QUALIFIED_RSSI_24G_BAND)
+                                     mThresholdQualifiedRssi5)
                                     || (ScanResult.is5GHz(mWifiInfo.getFrequency())
                                     && mWifiInfo.getRssi() >
                                     mThresholdQualifiedRssi5))) {
