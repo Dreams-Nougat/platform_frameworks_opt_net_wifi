8595de81db84704dd8a7d3eca47240678c4594f6
Paul Stewart <pstew@google.com>
2016-10-28 13:01:28 -0700

    WifiNotificationController: Explicitly ignore default case
    Indicate explicitly to the compiler that we don't care about
    anything but 3 states.
    
    Bug: 32490639
    Change-Id: I8091a93073ea8f85878471ebb5c01792f0f1e4f0
    Test: Compile only -- trivial change

--- service/java/com/android/server/wifi/WifiNotificationController.java
+++ service/java/com/android/server/wifi/WifiNotificationController.java
@@ -137,6 +137,8 @@ final class WifiNotificationController {
                                     case CAPTIVE_PORTAL_CHECK:
                                         resetNotification();
                                         break;
+                                    default:
+                                        break;
                                 }
                             }
                         } else if (intent.getAction().equals(
