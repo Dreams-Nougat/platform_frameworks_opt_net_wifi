b7a436a8f6f2803673cc93aaf15fc294073cbfb0
Glen Kuhne <kuh@google.com>
2016-12-08 10:27:31 -0800

    HIDLization of wifi framework
    Test: WIP
    Bug: 33383725
    Change-Id: I5414c2d6a1763a89adc226b8067166562e93d468

---
+++ service/java/com/android/server/wifi/WifiP2pNativeNew.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.server.wifi;
+/**
+ * Native calls sending requests to the P2P Hals, and callbacks for receiving P2P events
+ *
+ * {@hide}
+ */
+public class WifiP2pNativeNew {
+    private static final boolean DBG = false;
+    private static final String TAG = "WifiP2pNativeNew";
+}
