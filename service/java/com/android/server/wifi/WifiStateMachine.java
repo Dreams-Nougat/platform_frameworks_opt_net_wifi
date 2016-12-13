9923cb18112fbfe897d2f234a386a23ef198da4e
Glen Kuhne <kuh@google.com>
2016-12-12 16:51:35 -0800

    Implement bring up of HIDL supplicant ifaces
    Test: WIP
    Bug: 33383725
    Change-Id: Iff0fcdff8f038ad7ae67da88d65283d97e4855c3

--- service/java/com/android/server/wifi/WifiStateMachine.java
+++ service/java/com/android/server/wifi/WifiStateMachine.java
@@ -3724,6 +3724,7 @@ public class WifiStateMachine extends StateMachine implements WifiNative.WifiRss
                     break;
                 case CMD_BOOT_COMPLETED:
                     maybeRegisterNetworkFactory();
+                    mWifiNativeNew.registerForServiceNotification();
                     break;
                 case CMD_SCREEN_STATE_CHANGED:
                     handleScreenStateChanged(message.arg1 != 0);
@@ -4133,6 +4134,10 @@ public class WifiStateMachine extends StateMachine implements WifiNative.WifiRss
             if (mVerboseLoggingEnabled) {
                 logd("SupplicantStartedState enter");
             }
+
+            /*if (!mWifiNativeNew.startSupplicantHidl()) {
+                Log.e(TAG, "Failed to start HIDL Supplicant Interface");
+            }*/
 
             /* Wifi is available as long as we have a connection to supplicant */
             mNetworkInfo.setIsAvailable(true);
