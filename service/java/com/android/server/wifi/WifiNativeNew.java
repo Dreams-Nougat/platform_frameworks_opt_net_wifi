cbfabe4a48f9be40a61b60410159c95f5af55df3
Michael Plass <mplass@google.com>
2016-12-12 17:27:01 -0800

    Add chip bringup
    Test: WIP
    Bug: 33383725
    Change-Id: I0a0f32e377243444798e947adb2df56e19017920

--- service/java/com/android/server/wifi/WifiNativeNew.java
+++ service/java/com/android/server/wifi/WifiNativeNew.java
@@ -19,6 +19,8 @@ import android.hardware.wifi.V1_0.IWifi;
 import android.hardware.wifi.V1_0.IWifiChip;
 import android.hardware.wifi.V1_0.IWifiRttController;
 import android.hardware.wifi.V1_0.IWifiStaIface;
+import android.hardware.wifi.V1_0.IfaceType;
+import android.hardware.wifi.V1_0.StaBackgroundScanCapabilities;
 import android.hardware.wifi.V1_0.WifiStatus;
 import android.hardware.wifi.V1_0.WifiStatusCode;
 import android.hardware.wifi.supplicant.V1_0.ISupplicant;
@@ -26,6 +28,10 @@ import android.hardware.wifi.supplicant.V1_0.ISupplicantStaIface;
 import android.util.Log;
 
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
+
+//import android.os.RemoteException;
+
 /**
  * Native calls for bring up/shut down of the supplicant daemon and for
  * sending requests to the supplicant daemon
@@ -43,7 +49,8 @@ public class WifiNativeNew {
     private static final String HAL_HIDL_SERVICE_NAME = "wifi";
     private IWifi mHidlWifi;
     private     IWifiChip mHidlWifiChip;
-    private         IWifiStaIface mHidlWifiStaIface;
+    private         ArrayList<IWifiChip.ChipMode> mHidlWifiChipAvailableModes = null;
+    private         IWifiStaIface mIWifiStaIface;
     private         IWifiRttController mHidlWifiRttController;
 
     // Supplicant HAL HIDL interface objects
@@ -52,11 +59,16 @@ public class WifiNativeNew {
 
     /**
      * Bring up the HIDL Vendor HAL and configure for STA mode
-     * (CHIP CONFIGURATION CODE NOT YET COMPLETE)
      */
     public boolean startHidlHal() {
         /** Get the Wifi Service */
-        mHidlWifi = IWifi.getService(HAL_HIDL_SERVICE_NAME);
+        try {
+            mHidlWifi = IWifi.getService(HAL_HIDL_SERVICE_NAME);
+        } catch (NoSuchElementException e) {
+            Log.e(TAG, "HIDL service not registered: " + HAL_HIDL_SERVICE_NAME);
+//        } catch (RemoteException e) {
+//            Log.e(TAG, "HIDL service not running");
+        }
         if (mHidlWifi == null) {
             Log.e(TAG, "Failed to get Wifi HIDL interface objects.");
             return false;
@@ -67,7 +79,6 @@ public class WifiNativeNew {
             Log.e(TAG, "Starting wifi hal failed. Error code: " + status.code);
             return false;
         }
-
         /** List available Wifi chips */
         final ArrayList<Integer> chipIdsInternal = new ArrayList<>();
         // Use lambda's here.
@@ -85,7 +96,6 @@ public class WifiNativeNew {
             Log.e(TAG, "Expected 1 chip to be present, got: " + chipIdsInternal.size());
             return false;
         }
-
         /** Get chip 0 */
         // Use lambda's here.
         IWifi.getChipCallback chipCb = new IWifi.getChipCallback() {
@@ -102,41 +112,148 @@ public class WifiNativeNew {
             Log.e(TAG, "Failed to get WifiChip HIDL interface objects.");
             return false;
         }
-        /** Configure Chip Mode for STA**/
-            // see https://android-review.googlesource.com/#/c/310925/8/wifi/1.0/vts/functional/wifi_hidl_test_utils.cpp@224
-
-        /** Get Wifi STA Interface names */
-        final ArrayList<String> staIfNames = new ArrayList<>();
-        IWifiChip.getStaIfaceNamesCallback staIfaceNamesCb =
-                new IWifiChip.getStaIfaceNamesCallback() {
-            public void onValues(WifiStatus status, java.util.ArrayList<String> ifnames) {
+        /** Configure Chip Mode for STA */
+        mHidlWifiChip.getAvailableModes(new IWifiChip.getAvailableModesCallback() {
+            public void onValues(WifiStatus status, ArrayList<IWifiChip.ChipMode> modes) {
                 if (status.code != WifiStatusCode.SUCCESS) {
-                    Log.e(TAG, "Getting STA Iface Names failed. Error code: " + status.code);
+                    Log.e(TAG, "Getting chip modes failed. Error code: " + status.code);
                     return;
                 }
-                staIfNames.addAll(ifnames);
+                mHidlWifiChipAvailableModes = modes;
+            }
+        });
+        if (mHidlWifiChipAvailableModes == null) {
+            Log.e(TAG, "Failed to get WifiChip HIDL chip available modes.");
+            return false;
+        }
+        int modeId = -1;
+        int hits = 0;
+        int wantedType = IfaceType.STA;
+        for (IWifiChip.ChipMode mode : mHidlWifiChipAvailableModes) {
+            for (IWifiChip.ChipIfaceCombination combination : mode.availableCombinations) {
+                for (IWifiChip.ChipIfaceCombinationLimit ifaceLimit : combination.limits) {
+                    for (Integer ifaceType : ifaceLimit.types) {
+                        if (ifaceType.equals(wantedType)) {
+                            modeId = mode.id;
+                            hits++;
+                        }
+                    }
+                }
+            }
+        }
+        if (hits == 0) {
+            Log.e(TAG, "Failed to find matching mode");
+            return false;
+        }
+        if (hits > 1) {
+            Log.d(TAG, "number of matching modes: " + hits + ", picked modeId " + modeId);
+        }
+        WifiStatus wifiStatus = mHidlWifiChip.configureChip(modeId);
+        if (wifiStatus.code != WifiStatusCode.SUCCESS) {
+            Log.e(TAG, "configureChip failed. code: " + wifiStatus.code);
+        }
+
+        /** Create Wifi STA Interface */
+
+        IWifiChip.createStaIfaceCallback createStaIfaceCb =
+                new IWifiChip.createStaIfaceCallback() {
+            public void onValues(WifiStatus status, IWifiStaIface iface) {
+                if (status.code != WifiStatusCode.SUCCESS) {
+                    Log.e(TAG, "Create STA Iface failed. Error code: " + status.code);
+                    return;
+                }
+                mIWifiStaIface = iface;
             }
         };
-        mHidlWifiChip.getStaIfaceNames(staIfaceNamesCb);
-        if (staIfNames.size() != 1) {
-            Log.e(TAG, "Expected exactly 1 StaIface to be present, got: " + staIfNames.size());
+        mHidlWifiChip.createStaIface(createStaIfaceCb);
+        if (mIWifiStaIface == null) {
             return false;
         }
 
-        /** Get Wifi STA Interface */
-        IWifiChip.getStaIfaceCallback staIfaceCallback = new IWifiChip.getStaIfaceCallback() {
-            public void onValues(WifiStatus status, IWifiStaIface iface) {
-                if (status.code != WifiStatusCode.SUCCESS) {
-                    Log.e(TAG, "Getting STA Iface " + staIfNames.get(0) + " failed. Error code: "
-                            + status.code);
-                    return;
-                }
-                mHidlWifiStaIface = iface;
-            }
-        };
-
         Log.i(TAG, "Retrieved the HIDL interface objects.");
         return true;
+    }
+
+    /** Stops the HAL
+     * (SOMEDAY)
+     */
+    public void stopHal() {
+        // XXX
+    }
+
+    /** Tests whether the HAL is running or not
+     */
+    public boolean isHalStarted() {
+        return mIWifiStaIface != null;
+    }
+
+    /** Gets the interface index given a name
+     */
+    public int queryInterfaceIndex(String interfaceName) {
+        // XXX
+        return -1;
+    }
+
+    /** Gets the interface name given an index
+     *
+     * @param index of interface
+     * @return interface name, or null if index is unknown
+     */
+    public String getInterfaceName(int index) {
+        // XXX
+        return "notyet0";
+    }
+
+    /** Holds the result of getScanCapabilities */
+    public static class ScanCapabilities {
+        public int max_scan_cache_size;
+        public int max_scan_buckets;
+        public int max_ap_cache_per_scan;
+        public int max_rssi_sample_size;
+        public int max_scan_reporting_threshold;
+        public int max_hotlist_bssids;
+        public int max_significant_wifi_change_aps;
+        public int max_bssid_history_entries;
+        public int max_number_epno_networks;
+        public int max_number_epno_networks_by_ssid;
+        public int max_number_of_white_listed_ssid;
+    }
+
+    private boolean mOk; // why does a local not work for this?
+
+    /** Gets the scan capabilities
+     *
+     * @param capabilities object to be filled in
+     * @return true for success. false for failure
+     */
+    public boolean getScanCapabilities(ScanCapabilities capabilities) {
+        mOk = false;
+        ScanCapabilities out = capabilities;
+        if (!isHalStarted()) {
+            return false;
+        }
+        mIWifiStaIface.getBackgroundScanCapabilities(
+                new IWifiStaIface.getBackgroundScanCapabilitiesCallback() {
+                    public void onValues(
+                            WifiStatus status,
+                            StaBackgroundScanCapabilities capabilities) {
+                        if (status.code != WifiStatusCode.SUCCESS) return;
+                        out.max_scan_cache_size = capabilities.maxCacheSize;
+                        out.max_ap_cache_per_scan = capabilities.maxApCachePerScan;
+                        out.max_scan_buckets = capabilities.maxBuckets;
+                        out.max_rssi_sample_size = 0;
+                        out.max_scan_reporting_threshold = capabilities.maxReportingThreshold;
+                        out.max_hotlist_bssids = 0;
+                        out.max_significant_wifi_change_aps = 0;
+                        out.max_bssid_history_entries = 0;
+                        out.max_number_epno_networks = 0;
+                        out.max_number_epno_networks_by_ssid = 0;
+                        out.max_number_of_white_listed_ssid = 0;
+                        mOk = true;
+                    }
+                }
+        );
+        return mOk;
     }
 
     private boolean startSupplicantHal() {
