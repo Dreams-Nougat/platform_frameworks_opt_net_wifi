14874e1bc3642cb5e17f8a3e2025e03aeb1da4d7
Sohani Rao <sohanirao@google.com>
2016-12-20 14:33:38 -0800

    LogcatLog should use Verbose logging for trace API
    Update trace() API in WifiLog.java to use VERBOSE log level instead.
    Also, Wifi Verbose Logging setting from Developer settings will be
    used along with the log level for the message to determine if it will
    be flushed to the log buffer. Expose a new method to set the verbose
    logging mode in LogcatLog and synchronize access to the variable that
    saves this setting.
    
    Bug: 33783647
    Test: Unit test and verify logging
    Change-Id: I183f6c9fa148f241b2e9eba9846ccaf34f79ae70

--- service/java/com/android/server/wifi/LogcatLog.java
+++ service/java/com/android/server/wifi/LogcatLog.java
@@ -21,7 +21,6 @@ import android.util.Log;
 import com.android.internal.annotations.Immutable;
 
 import javax.annotation.concurrent.ThreadSafe;
-
 /**
  * Provides a WifiLog implementation which uses logd as the
  * logging backend.
@@ -33,9 +32,18 @@ import javax.annotation.concurrent.ThreadSafe;
 @Immutable
 class LogcatLog implements WifiLog {
     private final String mTag;
+    private static volatile boolean sVerboseLogging = false;
 
     LogcatLog(String tag) {
         mTag = tag;
+    }
+
+    public static void enableVerboseLogging(int verboseMode) {
+        if (verboseMode > 0) {
+            sVerboseLogging = true;
+        } else {
+            sVerboseLogging = false;
+        }
     }
 
     /* New-style methods */
@@ -168,7 +176,9 @@ class LogcatLog implements WifiLog {
             if (mNextFormatCharPos < mFormat.length()) {
                 mStringBuilder.append(mFormat, mNextFormatCharPos, mFormat.length());
             }
-            Log.println(mLogLevel, mTag, mStringBuilder.toString());
+            if (sVerboseLogging || mLogLevel > Log.DEBUG) {
+                Log.println(mLogLevel, mTag, mStringBuilder.toString());
+            }
         }
 
         /* Should generally not be used; implemented primarily to aid in testing. */
